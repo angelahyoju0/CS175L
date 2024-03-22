8f1f1802c96d1d2a1e1e4b81ef7f72a1c54e4194
angelahyoju0 <146225964+angelahyoju0@users.noreply.github.com>
2024-03-21 21:38:20 -0400

    This is my Average from Input File with Exception Handling assignment submission.

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,19 +1,24 @@
 #Angela An
 #CS 175L
-#Average from Input File
-
-
+#Average From Input File with Exception Handling
 
 def main():
     total = 0
     count = 0
 
-    with open('numbers.txt', 'r') as file:
-        for line in file:
-            number = float(line)
-            count = count + 1
-            total = total + number
-            print(f'I read in {count} number(s) Current number is: {number:8.2f} Total is: {total:8.2f}')
+    try:
+        with open('numbers.txt', 'r') as file:
+            for line in file:
+                try:
+                    number = float(line)
+                    count = count + 1
+                    total = total + number
+                    print(f'I read in {count} number(s) Current number is: {number:8.2f} Total is: {total:8.2f}')
+                except ValueError:
+                    print(f"Bad data: {line.strip()} skipping")
+    except IOError:
+        print("SystemExit: File not found: numbers.txt - exiting")
+        raise SystemExit
 
     if count > 0:
         average = total / count
